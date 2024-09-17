232578f4347cca445e1e400605fe87e1a8b6011f
Danil Shamsutdinov <a.danil.dark@gmail.com>
2024-09-17 05:12:29 +0300

    Calc int-day for data.
    refactor

--- src/data_preparation/process_nc.py
+++ src/data_preparation/process_nc.py
@@ -25,23 +25,32 @@ class PrepareDS(object):
 
     @staticmethod
     def unzip() -> None:
-        """Разархивировать .zip."""
+        """Разархивировать .zip в .nc"""
         for x in PrepareDS.path_to_data.glob("*.zip"):
             with ZipFile(x, "r") as myzip:
                 myzip.extractall(path=PrepareDS.path_to_data)
                 logging.info(f"Unziped {x.name}")
 
     def assemble_resulting_dataframe(self):
+        """
+        Обработка .nc файлов и сборка в результирующий .csv.
+        """
         for x in PrepareDS.path_to_data.glob("*.nc"):
+            # читаем .nc
             df = xr.open_dataset(x).to_dataframe()
+            # имя для преобразованных nc в csv
             name_for_csv: str = f"{str(x.name)[:-3]}.csv"
             df.to_csv(Path(PrepareDS.path_to_data, name_for_csv))
+            # читаем эти csv
             df_2 = pd.read_csv(Path(PrepareDS.path_to_data, name_for_csv))
+            # добавляем "level"
             df_2["level"] = x.name.split(".")[5].replace("l", "")
             # мгу 55.7, 37.5
             # ифа 55.7, 37.6
-            df_2 = df_2[(df_2['lat'] == 55.7) &
-                        ((df_2['lon'] == 37.5) | (df_2['lon'] == 37.6))]
+            # знс 55.7, 36.8
+            # отсекаем ненужные координаты
+            df_2 = df_2[(df_2['lat'] == 55.7) & ((df_2['lon'] == 37.5) | (df_2['lon'] == 36.8))]
+            # собираем конечный csv
             if self.result_df is None:
                 self.result_df = df_2
                 logging.info(f"init result_df; name = {name_for_csv}, shape = {self.result_df.shape}")
@@ -50,11 +59,16 @@ class PrepareDS(object):
                 logging.info(f"add to result_df; name = {name_for_csv}, shape = {self.result_df.shape}")
 
     def write_result_df(self):
-        # time,lat,lon,pm2p5, level
+        """
+        Пишем конечный csv [time,lat,lon,pm2p5,level].
+        """
+        # сортируем по времени, уровню
         self.result_df = self.result_df.sort_values(["time", "level"],
                                                     ascending=True)
-        self.result_df.to_csv(Path(PrepareDS.path_to_data, "result_df.csv"))
+        self.result_df.to_csv(Path(PrepareDS.path_to_data, "result.csv"),
+                              index=False)
         logging.info(f"write result_df; shape = {self.result_df.shape}")
+
 
 if __name__ == "__main__":
     PrepareDS.unzip()
